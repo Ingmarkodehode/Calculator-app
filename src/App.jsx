bc082091d883026216b166c68148e66ae5e16e3e
ingmarkodehode <ingmar.kodehode@gmail.com>
2023-10-04 12:39:12 +0200

    gettin there

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,100 @@ function App() {
     if (!ops.includes(value)) {
       setResult(eval(calc + value).toString());
     }
+    return;
   };
+  const createDitigs = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button
+          className={"grid" + [i]}
+          id={"grid" + [i]}
+          onClick={() => updateCalc(i.toString())}
+          key={i}
+        >
+          <p>{i}</p>
+        </button>
+      );
+    }
+    return digits;
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+  /* if calc = empty return else slice from first digit (0) (slice removes 1 since spesified -1) and setCalc becomes the new value*/
+  const deleteLast = () => {
+    if (calc == "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
+  return (
+    <>
+      <div id="app">
+        <div id="upper">
+          <p>Calc</p>
+          <h6>Theme</h6>
+        </div>
+        <div id="calculator">
+          <div id="display">
+            {result ? <span>({result})</span> : ""}&nbsp;
+            {calc || "0"}
+          </div>
+          <div className="grid-container">
+            <div className="buttons">
+              {/* sets up the divide, multible, pluss and minus button and updates the calc with the correct operator */}
+              {/* <div id="operators"> */}
+              <button className="divide" onClick={() => updateCalc("/")}>
+                <p>/</p>
+              </button>
+              <button className="multiply" onClick={() => updateCalc("*")}>
+                <p>x</p>
+              </button>
+              <button className="pluss" onClick={() => updateCalc("+")}>
+                <p>+</p>
+              </button>
+              <button className="minus" onClick={() => updateCalc("-")}>
+                <p>-</p>
+              </button>
+              {/* sets up the DEL key to remove last number */}
+              <button className="del" onClick={deleteLast}>
+                <p>DEL</p>
+              </button>
+              <button className="zero" onClick={() => updateCalc("0")}>
+                <p>0</p>
+              </button>
+              <button className="dot" onClick={() => updateCalc(".")}>
+                <p>.</p>
+              </button>
+              {/* </div> */}
+
+              {/* <div className="digits"> */}
+              {/* <div id="digits"> */}
+              {createDitigs()}
+              {/* sets up the 0 and . buttons */}
+
+              {/* sets up the = button with a onclick to the calculate function */}
+              <button className="reset">
+                <p>RESET</p>
+              </button>
+              <button className="equal" onClick={calculate}>
+                <p>=</p>
+              </button>
+              {/* </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default App;
 
 // function App() {
 //   const [count, setCount] = useState(0)
---
+++ src/testcode.jsx
@@ -0,0 +1,90 @@
+// {/* <>
+// <div id="app">
+//   <div id="upper">
+//   <p>Calc</p>
+//   <h6>Theme</h6>
+//   </div>
+//   <div id="calculator">
+//   <div id="display">
+//   {result ? <span>({result})</span> : ""}&nbsp;
+//   {calc || "0"}
+//     </div>
+//     <div className="grid-container">
+//       {/* sets up the divide, multible, pluss and minus button and updates the calc with the correct operator */}
+//       <div id="operators">
+//         <button onClick={() => updateCalc("/")}>/</button>
+//         <button onClick={() => updateCalc("*")}>*</button>
+//         <button onClick={() => updateCalc("+")}>+</button>
+//         <button onClick={() => updateCalc("-")}>-</button>
+//         {/* sets up the DEL key to remove last number */}
+//         <button onClick={deleteLast}>DEL</button>
+//       </div>
+
+// <div id="digits">
+//         {createDitigs()}
+//         {/* sets up the 0 and . buttons */}
+//         <button onClick={() => updateCalc("0")}>0</button>
+//         <button onClick={() => updateCalc(".")}>.</button>
+
+//         {/* sets up the = button with a onclick to the calculate function */}
+//         <button onClick={calculate}>=</button>
+//         <button>RESET</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+// </> */}
+
+// {/* <>
+//       <div id="app">
+//         <div id="upper">
+//           <p>Calc</p>
+//           <h6>Theme</h6>
+//         </div>
+//         <div id="calculator">
+//           <div id="display">
+//             {result ? <span>({result})</span> : ""}&nbsp;
+//             {calc || "0"}
+//           </div>
+//           <div className="grid-container">
+//             {/* sets up the divide, multible, pluss and minus button and updates the calc with the correct operator */}
+//             <div id="operators">
+//               <button className="grid-17" onClick={() => updateCalc("/")}>
+//                 /
+//               </button>
+//               <button className="grid-16" onClick={() => updateCalc("*")}>
+//                 *
+//               </button>
+//               <button className="grid-14" onClick={() => updateCalc("+")}>
+//                 +
+//               </button>
+//               <button className="grid-15" onClick={() => updateCalc("-")}>
+//                 -
+//               </button>
+//               {/* sets up the DEL key to remove last number */}
+//               <button className="grid-13" onClick={deleteLast}>
+//                 DEL
+//               </button>
+//               <button className="grid-0" onClick={() => updateCalc("0")}>
+//                 0
+//               </button>
+//               <button className="grid-10" onClick={() => updateCalc(".")}>
+//                 .
+//               </button>
+//             </div>
+
+//             <div className="digits">
+//             {/* <div id="digits"> */}
+//               {createDitigs()}
+//               {/* sets up the 0 and . buttons */}
+
+//               {/* sets up the = button with a onclick to the calculate function */}
+//               <button className="grid-12" onClick={calculate}>
+//                 =
+//               </button>
+//               <button className="grid-11">RESET</button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+// </>}
